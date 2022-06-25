f88e91ef360ead1ef36452b7ddc1ced6f2f696fe
brianjia2020 <brianjia1123@gmail.com>
2022-06-25 14:03:44 -0400

    initial commit

---
+++ source/04_const.cpp
@@ -0,0 +1,19 @@
+
+#define day 7
+
+// when defining a constant value,
+// 1. use define
+// 2. use const
+
+int main()
+{
+    // 1. use define
+    cout << "The total number of days in a week is: " << day << endl;
+
+    // 2. use const
+    const int month = 12;
+    cout << "The total number of months in a year is: " << month << endl;
+
+    system("pause");
+    return 0;
+}
